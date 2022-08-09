28a035d1c48071e7bd553fe19f633be677c94143
NimishVadhwa <vadhwanimish20@gmail.com>
2022-08-09 15:26:34 +0530

    'update'

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ require('dotenv').config()
 const port = process.env.PORT || 7100;
 const cors = require('cors');
 const route = require('./src/routes');
-
+const chat = require('./src/models/ChatModel');
+const room = require('./src/models/RoomModel');
+const { Op } = require("sequelize");
 
 app.use(express.json());
 app.use(cors());
-app.use("/assets/images", express.static("assets/images"));
+
 
 app.use('/api/', route);
 
@@ -20,6 +22,8 @@ app.use((req, res, next) => {
     next();
 })
 
+app.use("/assets", express.static("assets"));
+
 //error handling
 app.use((err, req, res, next) => {
     res.status(err.status || 500).json({ status: false, message: err.message });
@@ -29,8 +33,76 @@ app.use((err, req, res, next) => {
 db
     .sync()
     .then((result) => {
-        app.listen(port);
+        const server = app.listen(port);
+        const io = require('socket.io')(server, { 
+            cors: {
+                origin: "*",
+                methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+            } 
+            
+        });
+        
+        
+        io.on('connection',(socket) => {
+            console.log('socket id=======',socket.id);
+            
+            
+            socket.on('join-chat',async(data)=>{
+               
+               console.log('join chat===',data);
+               
+                socket.join(data.room_name);
+                
+                await chat.update({
+                    status : "read"
+                },{
+                    where:{ room_id : data.room_id }
+                })
+                
+                console.log('Room joined == ', socket.rooms);
+                
+                const clients = io.sockets.adapter.rooms.get(data.room_name);
+                
+                console.log(data.room_name+' room======>',clients)
+                
+            });
+            
+            
+            socket.on('send_chat',async(data)=>{
+                console.log('send====',data);
+                
+                let dta = await chat.create({
+                    message : data.message,
+                    file_type : data.file_type,
+                    room_name : data.room_name,
+                    room_id : data.room_id,
+                    status : "unread",
+                    sender_id : data.sender_id,
+                    receiver_id : data.receiver_id
+                });
+                
+                socket.broadcast.to(data.room_name).emit('receive_chat',dta);
+            })
+            
+            
+            socket.on('disconnects',async(data)=>{
+               
+                // socket.disconnect(data.room_name);
+                socket.leave(data.room_name);
+                
+                console.log('disconnet==',socket.id);
+                
+                console.log('ALl rooms======',    socket.rooms);
+            });
 
+            
+            //   console.log('ALl rooms======',    io.sockets.adapter.rooms);
+              
+              console.log('ALl rooms======',    socket.rooms);
+
+        });
+
+        
         console.log('Db connected===', port);
     }).catch((err) => {
         console.log('db not connected');
